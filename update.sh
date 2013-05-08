77c3aa8a8f5296fd3608d4bf338a422e5e148a1a
Mikko Ohtamaa <mikko@opensourcehacker.com>
2013-05-08 23:28:50 +0300

    Creating repo for shareable Sublime Text 2 setup

--- update.sh
+++ update.sh
@@ -2,7 +2,14 @@
 
 set -e
 
+# Various shareable settings files
 cp "/Users/mikko/Library/Application Support/Sublime Text 2/Packages/User/Package Control.sublime-settings" .
 cp "/Users/mikko/Library/Application Support/Sublime Text 2/Packages/User/Distraction Free.sublime-settings" .
 cp "/Users/mikko/Library/Application Support/Sublime Text 2/Packages/User/Preferences.sublime-settings" .
 
+# subl command for aliwas
+cp ~/tools/zsh-scripts/bin/subl-wrapper .
+
+# PythonTidy settings file used with PythonTidy plugin
+# (Line wrap disabled)
+cp ~/.pythontidy.xml .
