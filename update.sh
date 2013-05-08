8a376b21c43527a1991269b42ae9ef9867fb64de
Mikko Ohtamaa <mikko@opensourcehacker.com>
2013-05-08 23:26:47 +0300

    Added settings

---
+++ update.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+set -e
+
+cp "/Users/mikko/Library/Application Support/Sublime Text 2/Packages/User/Package Control.sublime-settings" .
+cp "/Users/mikko/Library/Application Support/Sublime Text 2/Packages/User/Distraction Free.sublime-settings" .
+cp "/Users/mikko/Library/Application Support/Sublime Text 2/Packages/User/Preferences.sublime-settings" .
+
